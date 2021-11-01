2f8271ccfd7545a7a9e4ffbe3286ef21801e4798
Luis Amarilla <flopydepapi@gmail.com>
2021-11-01 18:57:29 -0300

    primer commit 3f

---
+++ js/boton.js
@@ -0,0 +1,7 @@
+
+
+function cartelito() {
+  alert ("otro cartel");
+  document.getElementById("colorletra").style.color = "white";
+  document.getElementById("colorletra").style.backgroundColor = "black";
+}
